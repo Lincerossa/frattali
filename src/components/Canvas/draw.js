5d67859866cc4faeb5d9f8ae42a067709313aeeb
Lincerossa90 <marcello.luatti@alkemy.com>
2018-12-17 23:03:59 +0100

    [components] Canvas: corretta generazione di frattali

--- src/components/Canvas/draw.js
+++ src/components/Canvas/draw.js
@@ -20,34 +20,39 @@ function getCanvasPoint(point, canvasCenter){
 
 
 export const drawFrattali = ({ctx, line, frattali, canvasCenter}) => {
-          
+  
+  let frattaliLines = Array.from({length: frattali}, (e) => [])
+
   for (let p = 0; p < line.length; p++) {
-    // CARTESIANO/////////////
-    const { x, y } = getCartesianPoint(line[p], canvasCenter)
-    const radius = Math.sqrt(Math.abs(Math.pow(x, 2) + Math.pow(y,2)))
-    const alpha = Math.atan2(y, x) * 180 / Math.PI;
-    const pointAngle = alpha < 0 ? 180 + (180 + alpha) : alpha
+
+    const point = getCartesianPoint(line[p], canvasCenter)
+    const radius = Math.sqrt(Math.abs(Math.pow(point.x, 2) + Math.pow(point.y,2)))
+    const alpha = Math.atan2(point.y, point.x) * 180 / Math.PI;
+    const anglePointToCenter = alpha < 0 ? 180 + (180 + alpha) : alpha
     const angles = Array.from({length: frattali}, (e, index) =>( 360 / frattali)*(index + 1))
     
-    // beta: da rifattorizzare correttamente
-    
-    for (let i = 0; i < angles.length; i++) {
-      ctx.beginPath();
+    for (let i = 0; i < angles.length; i++) {  
       const frattaleAngle = angles[i];
-      const frattaleX = Math.cos((pointAngle - frattaleAngle)*Math.PI/180) * radius
-      const frattaleY = Math.sin((pointAngle - frattaleAngle )*Math.PI/180) * radius
-
+      const frattaleX = Math.cos((anglePointToCenter - frattaleAngle)*Math.PI/180) * radius
+      const frattaleY = Math.sin((anglePointToCenter - frattaleAngle )*Math.PI/180) * radius
       const frattalePoint = getCanvasPoint({
         x: frattaleX,
         y: frattaleY
       }, canvasCenter)
 
-      
-      ctx.moveTo(frattalePoint.x,frattalePoint.y);
-      ctx.lineTo(frattalePoint.x+1, frattalePoint.y+1)
-      ctx.strokeStyle="#ac00ff"
-     
-      ctx.stroke();
+      frattaliLines[i][p] = frattalePoint
     }
+  }
+
+  for (let i = 0; i < frattaliLines.length; i++) {
+    const frattaleLine = frattaliLines[i];
+    ctx.beginPath();
+    for (let d = 0; d < frattaleLine.length; d++) {
+      const frattalePoint = frattaleLine[d];
+      ctx.lineTo(frattalePoint.x,frattalePoint.y);
+      ctx.strokeStyle="#ac00ff"
+    }
+    ctx.stroke();
+    ctx.closePath();  
   }
 }
