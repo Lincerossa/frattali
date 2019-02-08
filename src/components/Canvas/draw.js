bad21dd3161ca8e26afc9f0ba68071771c6c1447
Lincerossa90 <marcello.luatti@alkemy.com>
2019-02-08 18:18:54 +0100

    [components] Canvas refactor

--- src/components/Canvas/draw.js
+++ src/components/Canvas/draw.js
@@ -1,63 +1,56 @@
+function getFrattaliLines({ divisions, points, center }) {
+  const frattaliLines = Array.from({ length: divisions }, e => []);
 
+  for (let p = 0; p < points.length; p++) {
+    const point = points[p];
 
+    const radius = Math.sqrt(Math.pow(point.x, 2) + Math.pow(point.y, 2));
+    const alpha = (Math.atan2(point.y, point.x) * 180) / Math.PI;
+    const anglePointToCenter = alpha < 0 ? 360 + alpha : alpha;
 
-export const drawLines =  ({ ctx, lines, center }) => {
+    for (let division = 0; division < divisions; division++) {
+      const frattaleAngle = (360 / divisions) * (division + 1);
+      const frattaleAlignedRadialAngle =
+        ((anglePointToCenter - frattaleAngle) * Math.PI) / 180;
 
-  for (let l = 0; l < lines.length; l++) {
-    const { divisions, color, thickness, points } = lines[l];  
+      const frattalePoint = {
+        x: Math.cos(frattaleAlignedRadialAngle) * radius,
+        y: Math.sin(frattaleAlignedRadialAngle) * radius,
+      };
 
-    // da rifattorizzare: molto pesante ma permette di ottenere le posizioni di ciascu pt speculare a quello inserito dall'utente
-
-    const frattaliLines = Array.from({ length: divisions }, e => []);
-
-
-    for (let p = 0; p < points.length; p++) {
-      const point = points[p]
-      const radius = Math.sqrt(Math.pow(point.x, 2) + Math.pow(point.y, 2));
-      const alpha = (Math.atan2(point.y, point.x) * 180) / Math.PI;
-      const anglePointToCenter = alpha < 0 ? 360 + alpha : alpha;
-
-      for (let division = 0; division < divisions; division++) {
-        
-        const frattaleAngle = (360 / divisions) * (division + 1)
-
-        const frattalePoint = {
-          x:  Math.cos(((anglePointToCenter - frattaleAngle) * Math.PI) / 180) *
-          radius,
-          y: Math.sin(((anglePointToCenter - frattaleAngle) * Math.PI) / 180) *
-          radius
-        }
-
-        frattaliLines[division][p] = {
-          x: frattalePoint.x + center.x,
-          y: center.y - frattalePoint.y,
-        };
-        
-      }
-    
+      frattaliLines[division][p] = {
+        x: frattalePoint.x + center.x,
+        y: center.y - frattalePoint.y,
+      };
     }
+  }
 
+  return frattaliLines;
+}
+
+export const drawLines = ({ ctx, lines, center }) => {
+  for (let l = 0; l < lines.length; l++) {
+    const { divisions, color, thickness, points } = lines[l];
+
+    const frattaliLines = getFrattaliLines({ divisions, points, center });
 
     for (let i = 0; i < frattaliLines.length; i++) {
       const frattaleLine = frattaliLines[i];
       ctx.beginPath();
       for (let f = 0; f < frattaleLine.length; f++) {
-        const frattalePoint = frattaleLine[f]
-        ctx.lineTo(frattalePoint.x, frattalePoint.y)
+        const frattalePoint = frattaleLine[f];
+        ctx.lineTo(frattalePoint.x, frattalePoint.y);
         ctx.lineWidth = thickness;
         ctx.strokeStyle = color;
       }
       ctx.stroke();
       ctx.closePath();
     }
-
   }
 };
 
-
-
-export const drawBackground = ({ ctx, backgroundColor, width, height}) => {
+export const drawBackground = ({ ctx, backgroundColor, width, height }) => {
   const ratio = window.devicePixelRatio;
   ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, width * ratio, height * ratio);
-}
+};
--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,54 +1,47 @@
 import React, { useEffect, useRef } from "react";
-import { useGetCenter } from '../../useHooks'
-import { drawBackground, drawLines } from './draw'
+import { useGetCenter } from "../../useHooks";
+import { drawBackground, drawLines } from "./draw";
 
-export default ({width, height, backgroundColor, hd, lines}) => {
+export default ({ width, height, backgroundColor, hd, lines }) => {
   const canvas = useRef(null);
   const { center } = useGetCenter(canvas);
 
   useEffect(
     () => {
-      if(center) {
-        drawBackground({ 
-          ctx: canvas.current.getContext("2d"), 
-          backgroundColor,
-          width,
-          height
-        })
-        drawLines({
-          ctx: canvas.current.getContext("2d"),
-          lines,
-          center,
-        })
-      }
-
-      return () => {
-        canvas.current.getContext("2d").clearRect(0, 0, width, height);
-      }
-    }
-    ,
-    [center, lines, backgroundColor]
-  );
-
-  useEffect(
-    () => {
-      if(hd){
+      if (hd) {
         canvas.current.style.width = `${window.innerWidth}px`;
         canvas.current.style.height = `${window.innerHeight}px`;
         const ratio = window.devicePixelRatio;
         canvas.current.width = width * ratio;
         canvas.current.height = height * ratio;
         canvas.current.getContext("2d").scale(ratio, ratio);
-        drawBackground({ 
-          ctx: canvas.current.getContext("2d"), 
-          backgroundColor,
-          width,
-          height
-        })
       }
     },
-    [canvas.current, width, height]
+    [canvas.current, width, height, hd]
   );
 
-  return <canvas id="canvas" width={width} height={height} ref={canvas} />
-}
+  useEffect(
+    () => {
+      if (center) {
+        drawBackground({
+          ctx: canvas.current.getContext("2d"),
+          backgroundColor,
+          width,
+          height,
+        });
+        drawLines({
+          ctx: canvas.current.getContext("2d"),
+          lines,
+          center,
+        });
+      }
+
+      return () => {
+        canvas.current.getContext("2d").clearRect(0, 0, width, height);
+      };
+    },
+    [center, lines, backgroundColor, hd]
+  );
+
+  return <canvas id="canvas" width={width} height={height} ref={canvas} />;
+};
