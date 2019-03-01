cd5f2a97a339517cfc43e97fcf66681caf324987
Lincerossa90 <marcello.luatti@alkemy.com>
2019-03-01 14:23:09 +0100

    [config] Docker + sequelize

--- src/components/Wecome/index.js
+++ src/components/Wecome/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import * as S from "./styles";
 
-export default ({ nickname, picture }) => (
-  <S.Welcome>
+export default ({ nickname, picture, timer }) => (
+  <S.Welcome timer={timer}>
     <S.Picture backgroundImage={picture}>
       <S.Nickname>{nickname}</S.Nickname>
     </S.Picture>
