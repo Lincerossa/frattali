7f4469f9850d0c123ee9d9b2e01d4c9280a77902
Lincerossa90 <marcello.luatti@alkemy.com>
2019-03-04 19:08:16 +0100

    [backend] file configurazione (env) + deploy su now

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -4,17 +4,23 @@ const mongoose = require("mongoose");
 
 const schema = require("./graphql");
 
-mongoose.connect("mongodb://localhost:27017");
+require("dotenv").config({
+  path: `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ""}`,
+});
+
+mongoose.connect(process.env.MONGOOSE_URI);
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   const server = new ApolloServer({
     schema,
+    playground: true,
+    introspection: true,
   });
 
   const app = express();
   server.applyMiddleware({ app });
-
   app.listen({ port: 4000 }, () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
   );
