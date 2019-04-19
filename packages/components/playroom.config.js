9ca888aac6ca581acea2692fb8608ffe6b1d0ffc
Lincerossa90 <marcello.luatti@alkemy.com>
2019-04-19 13:49:32 +0200

    [components] storybook + playground 🏅

---
+++ packages/components/playroom.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  components: "./src",
+  outputPath: "./dist/playroom",
+
+  // Optional:
+  title: "Playroom testing",
+  widths: [320, 375, 768, 1024],
+  port: 9000,
+  openBrowser: true,
+  exampleCode: `
+    <Button>
+      Hello World!
+    </Button>
+  `,
+};
