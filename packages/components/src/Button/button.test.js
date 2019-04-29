953ab6f793e394ec2b4b18a061f6de71ffac1d72
Lincerossa90 <marcello.luatti@alkemy.com>
2019-04-29 16:24:02 +0200

    [components] button test: prova simulazione click

--- packages/components/src/Button/button.test.js
+++ packages/components/src/Button/button.test.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { render, mount, shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 
 import Button from './index'
 
 describe('<Button />', () => {
   it('Component should be rendered', () => {
-    const CC = mount(<Button>button</Button>)
-    expect(CC.exists()).toBe(true)
+    const tree = mount(<Button>button</Button>)
+    expect(tree.exists()).toBe(true)
   })
 
   it('Component should be have a string as children', () => {
-    const text = 'test string'
-    const CC = mount(<Button>{text}</Button>)
-    expect(CC.text()).toBe(text)
+    const tree = mount(<Button>test string</Button>)
+    expect(tree.text()).toBe('test string')
+  })
+
+  it('Simulate the click event', () => {
+    const Foo = jest.fn().mockReturnValue('fired function')
+
+    const tree = shallow(<Button onClick={Foo} />)
+
+    // trying to call it
+    expect(Foo).toBeCalled()
+
+    // test the returned value
+    expect(tree.prop('onClick')()).toBe('fired function')
   })
 })
