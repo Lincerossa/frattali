01cda275b6497bf46c5e9471729dd1753572792f
Lincerossa90 <marcello.luatti@alkemy.com>
2019-05-03 22:12:43 +0200

    [components] Label nuovo componente

---
+++ packages/components/src/Label/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import * as S from './styles'
+
+export default ({ children }) => <S.Label>{children}</S.Label>
---
+++ packages/components/src/Label/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Label = styled.label`
+  color: white;
+  display: block;
+  font-family: serif;
+  font-size: 1.25rem;
+  margin-bottom: 0.75rem;
+  letter-spacing: 0.04rem;
+  text-transform: uppercase;
+`
