21b08f4d115a2b494610a99659df1ef40a048cc2
Lincerossa90 <marcello.luatti@alkemy.com>
2019-05-03 18:05:45 +0200

    [frontend] Routes file semplificato

--- packages/frontend/src/Routes.js
+++ packages/frontend/src/Routes.js
@@ -1,55 +1,24 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Art, LoadUser, WelcomeUser } from './views'
-import * as actions from './Redux/auth/actions'
-import { isAuthenticated } from './Redux/auth/reducer'
-import Auth from './Auth/Auth'
 
-const AuthenticationRouteHandler = props => {
-  const { children, isAuthenticated } = props
+import { AuthenticationRouteHandler } from './modules'
 
-  if (isAuthenticated) {
-    return children
-  }
-  if (!isAuthenticated && window.location.hash.indexOf('access_token') > -1) {
-    return (
-      <Redirect
-        to={{
-          pathname: '/loaduser',
-          hash: window.location.hash,
-        }}
-      />
-    )
-  }
-  if (!isAuthenticated) {
-    const instantiateAuth = new Auth()
-    instantiateAuth.login()
-  }
-
-  return null
+const withAuth = Component => props => {
+  return (
+    <AuthenticationRouteHandler>
+      <Component {...props} />
+    </AuthenticationRouteHandler>
+  )
 }
-
-const ConnectedAuthenticationRouteHandler = connect(
-  state => ({
-    isAuthenticated: isAuthenticated(state),
-  }),
-  actions
-)(AuthenticationRouteHandler)
-
-const PrivateRoute = props => (
-  <ConnectedAuthenticationRouteHandler>
-    <Route {...props} />
-  </ConnectedAuthenticationRouteHandler>
-)
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/loaduser" component={LoadUser} />
-      <PrivateRoute path="/welcomeuser" component={WelcomeUser} />
-      <PrivateRoute path="/" component={Art} />
+      <Route path="/welcomeuser" component={withAuth(WelcomeUser)} />
+      <Route path="/" component={withAuth(Art)} />
     </Switch>
   </BrowserRouter>
 )
