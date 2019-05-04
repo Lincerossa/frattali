d08316260f5ddb585ea0b92cdea6beccf49e1e40
Lincerossa90 <marcello.luatti@alkemy.com>
2019-05-04 23:09:58 +0200

    [components] Divider nuovo componente + Label esportata

---
+++ packages/components/src/Divider/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import * as S from './styles'
+
+export default ({ children }) => <S.Divider>{children}</S.Divider>
---
+++ packages/components/src/Divider/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Divider = styled.div`
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  font-family: sans-serif;
+  font-size: 1.125rem;
+  border-bottom: 1px solid #ffffff3b;
+  &:last-of-type {
+    border-bottom: none;
+  }
+`
