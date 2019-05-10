8d407c1480f48b682aaecddcaf3c482208899c69
Lincerossa90 <marcello.luatti@alkemy.com>
2019-05-10 14:17:01 +0200

    [frontend] -test- ApolloProvider setta un header con il token di auth

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -6,23 +6,38 @@ import { PersistGate } from 'redux-persist/integration/react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { store, persistor } from './Redux'
+import { getAccessToken } from './Redux/auth/reducer'
 import GlobalStyle from './styles/global'
 import theme from './styles/theme'
 import * as serviceWorker from './serviceWorker'
 import Routes from './Routes'
 
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URI,
-})
+function ApolloThing({ children }) {
+  const storeState = store.getState()
+
+  const client = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_URI,
+    request: operation => {
+      operation.setContext(context => ({
+        headers: {
+          ...context.headers,
+          authorization: getAccessToken(storeState),
+        },
+      }))
+    },
+  })
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
 
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <ApolloProvider client={client}>
+        <ApolloThing>
           <Routes />
-        </ApolloProvider>
+        </ApolloThing>
       </ThemeProvider>
     </PersistGate>
   </Provider>
